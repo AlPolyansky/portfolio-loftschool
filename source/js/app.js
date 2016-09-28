e402d5b763290925b183d25e80481413009310de
Eclerr <eclerrrr@yandex.ru>
2016-09-28 06:12:42 +0300

    create home page fix

--- source/js/app.js
+++ source/js/app.js
@@ -3,6 +3,12 @@ $( document ).ready(function() {
 	// TEST
 
 	$(".entry").on("click",function(){
+		$(this).hide();
+		$(".cover__content").toggle();
+	})
+
+	$(".cover__content__form a").on("click",function(){
+		$(".entry").show();
 		$(".cover__content").toggle();
 	})
 
