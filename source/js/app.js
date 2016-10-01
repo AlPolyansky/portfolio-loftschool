a9623c7fdfc85f205c05dc273750de3f3d3ed4be
Eclerr <eclerrrr@yandex.ru>
2016-10-01 05:03:51 +0300

    create page about

--- source/js/app.js
+++ source/js/app.js
@@ -2,6 +2,8 @@ $( document ).ready(function() {
     
 	// TEST
 
+
+
 	$(".entry").on("click",function(){
 		$(this).hide();
 		$(".cover__content").toggle();
@@ -11,5 +13,28 @@ $( document ).ready(function() {
 		$(".entry").show();
 		$(".cover__content").toggle();
 	})
+
+
+	/*var $skill = $('.skill'),
+   percent = parseInt($skill.data('percent')),
+   deg = 360*percent/100;
+	  if (percent > 50) {
+	    $skill.addClass('gt-50');
+	  }
+  $('.skill__circle__mask').css('transform','rotate('+ deg +'deg)');*/
+
+var $skill = $('.skill');
+  $skill.on("mouseenter",function(){
+  	
+   var percent = parseInt($skill.data('percent'));
+   var deg = 360*percent/100;
+	  if (percent > 50) {
+	  	setTimeout(function(){
+	  		$skill.addClass('gt-50');
+	  	},520);
+	    
+	  }
+  	$('.skill__circle__mask').css('transform','rotate('+ deg +'deg)');
+  })
 
 });
